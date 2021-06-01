1203a04cd9d2cee81c09812cdaebf96e6a258c54
ch3ky <85183368+ch3ky@users.noreply.github.com>
2021-06-01 19:37:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,168 @@
+const Discord = require("discord.js");
+const tokenfile = require("./tokenfile.json");
+const botconfig = require("./botconfig.json");
+const bot = new Discord.Client({disableEveryone: true});
+var weather = require('weather-js');
+                                                                                                               //státuszok
+bot.on("ready", async() => {
+    console.log(`${bot.user.username} elindult!`)
+
+    let státuszok = [
+        "Prefix: !",
+        "Készítő: ch3ky",
+        "menő :D"
+    ]
+
+    setInterval(function() {
+        let status = státuszok[Math.floor(Math.random()* státuszok.length)]
+
+        bot.user.setActivity(status, {type: "WATCHING"})
+    }, 3000)
+})
+                                                                                                            // szöveg válasz
+bot.on("message", async message => {
+    let MessageArray = message.content.split(" ");
+    let cmd = MessageArray[0];
+    let args = MessageArray.slice(1);
+    let prefix = botconfig.prefix;
+
+    if(cmd === `${prefix}hello`){
+        message.channel.send("szia");
+    }
+
+    if(cmd === `${prefix}help`){
+        let TesztEmbed = new Discord.MessageEmbed()
+        .setAuthor(message.author.username)
+        .setTitle("hamarosan...")
+        .addField("dolgok:", "Kick")
+        .setThumbnail(message.author.displayAvatarURL())
+        .setImage(message.guild.iconURL())
+
+        message.channel.send(TesztEmbed)
+    }
+
+    if(cmd === `${prefix}szöveg`){
+        let szöveg = args.join(" ");
+
+        if(szöveg) {
+            let dumaEmbed = new Discord.MessageEmbed()
+        .setColor("#98AA12")
+        .setAuthor(message.author.username)
+        .addField("Szöveg:", szöveg)
+        .setFooter(`${botname} | ${message.createdAt}`)
+    
+        message.channel.send(dumaEmbed)
+        } else {
+            message.reply("írj szöveget!")
+        }
+
+    }
+                                                                                                     //kick-ban
+    if(cmd === `${prefix}kick`){
+        let kick_user = message.mentions.members.first();
+        if(args[0] && kick_user){
+            
+
+            if(args[1]){
+
+                let KickEmbed = new Discord.MessageEmbed()
+                .setTitle("KICK")
+                .setColor("RED")
+                .setDescription(`**Kickelte:** ${message.author.tag}\n**Kickelve lett:** ${kick_user.user.tag}\n**Kick indoka:** ${args.slice(1).join(" ")}`)
+
+            message.channel.send(KickEmbed);
+
+                kick_user.kick(args.slice(1).join(" "));
+
+            } else {
+            let parancsEmbed = new Discord.MessageEmbed()
+            .setTitle("Parancs használata:")
+            .addField(`\`${prefix}kick <@név> [indok]\``, "˘˘˘")
+            .setColor("BLUE")
+            .setDescription("HIBA: Kérlek adj meg egy indokot!!")
+
+            message.channel.send(parancsEmbed);
+            }
+
+        } else {
+            let parancsEmbed = new Discord.MessageEmbed()
+            .setTitle("Parancs használata:")
+            .addField(`\`${prefix}kick <@név> [indok]\``, "˘˘˘")
+            .setColor("BLUE")
+            .setDescription("HIBA: Kérlek említs meg egy embert!")
+
+            message.channel.send(parancsEmbed);
+
+        }
+    }
+
+    if(cmd === `${prefix}ban`){
+        let ban_user = message.mentions.members.first();
+        if(args[0] && ban_user){
+            if(message.member.hasPermission("BAN_MEMBERS"))
+
+            
+            if(args[1]){
+
+                let BanEmbed = new Discord.MessageEmbed()
+                .setTitle("BAN")
+                .setColor("RED")
+                .setDescription(`**Banolta:** ${message.author.tag}\n**Banolva lett:** ${kick_user.user.tag}\n**Ban indoka:** ${args.slice(1).join(" ")}`)
+
+            message.channel.send(KickEmbed);
+
+                ban_user.ban(args.slice(1).join(" "));
+
+            } else {
+            let parancsEmbed = new Discord.MessageEmbed()
+            .setTitle("Parancs használata:")
+            .addField(`\`${prefix}ban <@név> [indok]\``, "˘˘˘")
+            .setColor("BLUE")
+            .setDescription("HIBA: Kérlek adj meg egy indokot!!")
+
+            message.channel.send(parancsEmbed);
+            }
+
+        } else {
+            let parancsEmbed = new Discord.MessageEmbed()
+            .setTitle("Parancs használata:")
+            .addField(`\`${prefix}ban <@név> [indok]\``, "˘˘˘")
+            .setColor("BLUE")
+            .setDescription("HIBA: Kérlek említs meg egy embert!")
+
+            message.channel.send(parancsEmbed);
+
+        }
+    }
+                                                                                            //időjárás
+    if(cmd === `${prefix}weather`){
+        if(args[0]){
+            weather.find({search: args.join(" "), degreeType: "C"}, function(err, result) {
+                if (err) message.reply(err);
+
+                if  (result.lenght === 0){
+                   message.reply("Kérlek adj meg egy létező település nevet!")
+                   return;
+                }
+
+                let current = result[0].current;
+                let location = result[0].location;
+
+                let WeaatherEmbed = new Discord.MessageEmbed()
+                .setDescription(`**${current.skytext}`)
+            })
+
+       } else {
+           message.reply("Kérlek adj meg egy település nevet!")
+       }
+    }   
+
+
+})
+
+
+
+
+
+
+bot.login(tokenfile.token);
